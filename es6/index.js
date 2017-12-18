3534642159c420667df80c0b5f377b4d8c0398f4
Mountain1210 <mountain_wang@126.com>
2017-12-18 10:04:46 +0800

    增加经纬 度

--- es6/index.js
+++ es6/index.js
@@ -22,3 +22,34 @@ var l = new leo;
 
 l.hello();
 
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')';
+  }
+}
+
+var a=new Point(10,20);
+console.log(a.toString());
+
+//////////////////获取经纬 度后，然后传给百度地图或者高德地图/////////////////////
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude);
+}
+console.log("===========================");
+getLocation()
+
