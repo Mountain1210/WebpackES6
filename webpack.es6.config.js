95faf1e2456609c9d470c69089cc0bd90d9542a7
Mountain1210 <mountain_wang@126.com>
2017-12-21 17:22:38 +0800

    es6.config中添加了scss的loader

--- webpack.es6.config.js
+++ webpack.es6.config.js
@@ -1,4 +1,5 @@
 var htmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require("path");
 module.exports = {
 	entry:{
@@ -24,15 +25,27 @@ module.exports = {
 			{
 				test:/.html$/,
 				loader:'html-loader'
-			}
+			},
+			//解析.scss文件,对于用 import 或 require 引入的sass文件进行加载，以及<style lang="sass">...</style>声明的内部样式进行加载
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style", 'css!sass') //这里用了样式分离出来的插件，如果不想分离出来，可以直接这样写 loader:'style!css!sass'
+            }
 		]
 	},
 	devServer:{
 
 	},
 	resolve:{
-		extensions:['','.js',".css",'jsx']  //自动补全识别后缀
+		extensions:['','.js',".css",'jsx','vue']  //自动补全识别后缀
 	},
+	 vue: {
+        loaders: {
+            js: 'babel', 
+            css: ExtractTextPlugin.extract("css"),
+            sass: ExtractTextPlugin.extract("css!sass")            
+        },
+    },
 	plugins:[
 		new htmlWebpackPlugin({
 			filename: 'index.html',
@@ -41,4 +54,5 @@ module.exports = {
 
 		})
 	]
+	
 }
